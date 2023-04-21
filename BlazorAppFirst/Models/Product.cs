ad4f4d5815f73197cc43a4093455a93f4a5086c9
boomcrash <alexmol_14@outlook.com>
2023-04-21 16:31:45 -0500

    cambios en front

--- BlazorAppFirst/Models/Product.cs
+++ BlazorAppFirst/Models/Product.cs
@@ -7,7 +7,18 @@
         public decimal? Price { get; set; }
         public string Description { get; set; }
         public int? CategoryId { get; set; }
-        public string[] Images { get; set; }
+        public string[]? Images { get; set; }
+        public string? Image { get; set; }
+
+    }
+
+    public class Product2
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal? Price { get; set; }
+        public string Description { get; set; }
+        public int? CategoryId { get; set; }
         public string? Image { get; set; }
 
     }
